3de2e8783593dffd109e858d29ec9c4bc5f7cf8b
Miki Horiuchi <miki.avenir.1123@gmail.com>
2012-10-17 17:55:43 +0900

    for metadata management using DB (on going)

--- mogami/metadata.py
+++ mogami/metadata.py
@@ -7,7 +7,7 @@ import errno
 
 
 class MogamiMetaFS(object):
-    """Class for managing metadata on local file system (like ext4).
+    """Class for managing metadata on local file system (like ext3).
 
     supposed to be used in metadata server's daemon.
     """
@@ -33,8 +33,7 @@ class MogamiMetaFS(object):
             return (None, None, None)
 
     def access(self, path, mode):
-        
-        pass
+        return os.access(self.rootpath + path, mode)
 
     def getattr(self, path):
         """
@@ -49,7 +48,7 @@ class MogamiMetaFS(object):
             fsize = -1
         return (st, fsize)
 
-    def readdir(self, path):
+    def listdir(self, path):
         return os.listdir(self.rootpath + path)
 
     def mkdir(self, path):
@@ -59,22 +58,30 @@ class MogamiMetaFS(object):
         return os.rmdir(self.rootpath + path)
     
     def unlink(self, path):
-        pass
+        return os.unlink(self.rootpath + path)
             
     def rename(self, oldpath, newpath):
-        pass
+        return os.rename(self.rootpath + oldpath, self.rootpath + newpath)
 
-    def chmopd(self, path, mode):
-        pass
+    def chmod(self, path, mode):
+        return os.chmod(self.rootpath + path, mode)
 
     def chown(self, path, uid, gid):
-        pass
+        return os.chown(self.rootpath + path, uid, gid)
 
     def truncate(self, path, size):
-        pass
+        try:
+            with open(self.rootpath + path, 'r+') as f:
+                (dest, path, org_size) = f.read().rsplit(',')
+                f.truncate(0)
+                f.seek(0)
+                f.write("%s,%s,%d" % (dest, path, size))
+        except IOError, e:
+            return e.errno
+        return 0
 
     def utime(self, path, times):
-        pass
+        return os.utime(path, times)
 
 
 class MogamiMetaDB(object):
@@ -90,10 +97,11 @@ class MogamiMetaDB(object):
         self.db_conn = sqlite3.connect(self.db_path)
         self.db_cur = self.db_conn.cursor()
     
-        # Create files table
+        # create a table for files
         self.db_cur.execute("""
-        CREATE TABLE files (
+        CREATE TABLE file (
         path TEXT PRIMARY KEY,
+        par_dir TEXT,
         mode INT,
         uid INT,
         gid INT,
@@ -102,29 +110,22 @@ class MogamiMetaDB(object):
         atime INT,
         mtime INT,
         ctime INT,
-        dist TEXT,
-        dist_path TEXT
+        dest TEXT,
+        dest_path TEXT
         )
         """)
-
-    def _set_file(self, path, st, dist, dist_path):
-        """register metadata of files to DB
-
-        @param path path name
-        @param st same with the return value of os.lstat(path)
-        @param dist ファイルを持っているメタデータサーバのIP
-        @param dist_path メタデータサーバ上でのpath
-        @return On success, zero is returned. On error, error number is returned.
-        """
-        r = self.db_cur.execute("SELECT * FROM files WHERE path = ?;", (path,)).fetchone()
+        
+    def _set_file(self, path, mode, uid, gid, ulink, size, atime, mtime,
+                  ctime, dest, dest_path):
+        r = self.db_cur.execute("SELECT * FROM file WHERE path = ?;", (path,)).fetchone()
         if r:
             return errno.EEXIST
         self.db_cur.execute("""
-        INSERT INTO files (
-        path, mode, uid, gid, nlink, size, atime, mtime, ctime, dist, dist_path) 
+        INSERT INTO file (
+        path, mode, uid, gid, nlink, size, atime, mtime, ctime, dest, dest_path) 
         VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-        """, (path, st.st_mode, st.st_uid, st.st_gid, st.st_nlink, st.st_size,
-              st.st_atime, st.st_mtime, st.st_ctime, dist, dist_path ))
+        """, (path, mode, uid, gid, nlink, size,
+              atime, mtime, ctime, dest, dest_path))
         self.db_conn.commit()
         return 0
 
@@ -140,7 +141,7 @@ class MogamiMetaDB(object):
         """
         self.db_cur.execute("""
             INSERT INTO dirs (
-            path, mode, uid, """)
+            path, mode, uid, gid, ulink, size, atime, mtime, ctime,""")
 
     def _rm_dir(self, path):
         r = self.db_cur.execute("", (path))
@@ -149,14 +150,14 @@ class MogamiMetaDB(object):
         """print information of all files
         """
         self.db_cur.execute("""
-        SELECT * FROM files""")
+        SELECT * FROM file""")
         l = self.db_cur.fetchall()
         for tmp in l:
             for tmp2 in tmp:
                 print tmp2,
             print ""
     
-    def return_st(self, path):
+    def _return_st(self, path):
         """return metadata statement of a file
 
         this function can be used as os.lstat
@@ -166,7 +167,7 @@ class MogamiMetaDB(object):
         """
         self.db_cur.execute("""
         SELECT mode, uid, gid, nlink, size, atime, mtime, ctime 
-        FROM files WHERE path = ?""", (path, ))
+        FROM file WHERE path = ?""", (path, ))
         l = self.db_cur.fetchall()
         if len(l) != 1:
             return None  # the file doesn't exist
@@ -178,15 +179,15 @@ class MogamiMetaDB(object):
             i += 1
         return st
 
-    def return_dist(self, path):
+    def _return_dest(self, path):
         """will return which data server has required file.
 
         @param path path of required file
         @return [ip_of_data_server, file_path_on_the_data_server]
         """
         self.db_cur.execute("""
-        SELECT dist, dist_path 
-        FROM files WHERE path = ?""", (path, ))
+        SELECT dest, dest_path 
+        FROM file WHERE path = ?""", (path, ))
         l = self.db_cur.fetchall()
         if len(l) != 1:
             return None
@@ -194,38 +195,97 @@ class MogamiMetaDB(object):
         return ret
 
     def access(self, path, mode):
+        """
+        This function should return either True or False.
+        @param path path of file to look for
+        @param mode one of os.F_OK, os.R_OK, os.W_OK and os.X_OK
+        """
+        self.db_cur.execute("""
+        SELECT mode, uid, gid FROM file WHERE path = ?""", (path))
+        l = self.db_cur.fetchall()
+        if len(l) != 1:
+            return False
+        if mode == F_OK:
+            return True
+
+        # check file mode and access mode
         pass
 
     def getattr(self, path):
-        pass
+        self.db_cur.execute("""
+        SELECT mode, uid, gid, nlink, size, atime, mtime, ctime 
+        FROM file WHERE path = ?""", (path))
+        l = self.db_cur.fetchall()
+        if len(l) != 1:
+            self._raise_with_error(errno.ENOENT)
+        st_org = l[0]
+        st = tips.MogamiStat()
+        i = 0
+        for attr in st.mogami_attrs:
+            setattr(st, attr, st_org[i])
+            i += 1
+        return st
 
-    def readdir(self, path):
-        pass
+    def listdir(self, path):
+        self.db_cur.execute("""
+        SELECT path FROM file WHERE par_dir = ?""", (path))
 
     def mkdir(self, path):
         pass
 
     def rmdir(self, path):
+        self.db_cur.execute("""
+        SELECT mode, uid, gid, nlink, size, atime, mtime, ctime 
+        FROM file WHERE path = ?""", (path))
+        l = self.db_cur.fetchall()
+        if len(l) != 1:
+            self._raise_with_error(errno.ENOENT)
+
         pass
     
     def unlink(self, path):
+        self.db_cur.execute("""
+        SELECT mode, uid, gid, nlink, size, atime, mtime, ctime 
+        FROM file WHERE path = ?""", (path))
+        l = self.db_cur.fetchall()
+        if len(l) != 1:
+            self._raise_with_error(errno.ENOENT)
+
         pass
             
     def rename(self, oldpath, newpath):
+        ## extract a record and update (path)
+        self.db_cur.execute("""
+        SELECT mode, uid, gid, nlink, size, atime, mtime, ctime 
+        FROM file WHERE path = ?""", (path))
+        l = self.db_cur.fetchall()
+        if len(l) != 1:
+            self._raise_with_error(errno.ENOENT)
+
         pass
 
     def chmod(self, path, mode):
+        ## extract a record and update (mode)
         pass
 
     def chown(self, path, uid, gid):
+        ## extract a record and update (uid, gid)
         pass
 
-    def truncate(self, path, uid, gid):
-        pass
+    def truncate(self, path, length):
+        ## extract a record and update (size)
 
     def utime(self, path, times):
+        """
+        @path file path to modify times
+        @param times tuple of (atime, mtime)
+        """
         pass
 
+    def _raise_with_error(self, num):
+        e = os.error(os.strerror(num))
+        e.errno = num
+        raise e
 
 if __name__ == '__main__':
     import doctest
